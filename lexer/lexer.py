3a1168336431dd09d6e4dee6467ec4b689e7b69b
Alex Solonenko <alex.solonencko@gmail.com>
2019-03-27 15:33:57 +0200

    Simple tokenizer

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -1,9 +1,9 @@
 TokenType = {
     'NUMBER': 'NUMBER',
-    'PLUS' : '+', 
-    'MINUS' : '-', 
-    'MULTIPLY' : '*', 
-    'DIVIDE' : '/',
+    '+' : 'PLUS', 
+    '-' : 'MINUS', 
+    '*' : 'MULTIPLY', 
+    '/' : 'DIVIDE',
     'EOF': 'EOF',
 }
 
@@ -11,24 +11,58 @@ class Token:
     def __init__(self, token_type, text):
         self.token_type = token_type
         self.text = text
+    
+    def __str__(self):
+        return f"TOKEN({self.token_type}, {self.text})"
+    
+    def __repr__(self):
+        return self.__str__()
 
 class Lexer:
+    OPERATORS = ('+', '-', '*', '/')
+    
     def __init__(self, text):
         self.text = text
-        self.text_lenght = len(self.text)
         self.position = 0
         self.tokens = []
+
+    @property
+    def current_char(self):
+        if self.position < len(self.text):
+            return self.text[self.position]
+        else:
+            return '\0'
     
     def tokenize(self):
-        pass
+        while self.position < len(self.text):
+            if self.current_char.isdigit():
+                self.tokenize_number()
+            elif self.current_char in self.OPERATORS:
+                self.tokenize_operator()
+            else:
+                self.next_char()
 
-    def next(self):
+        return self.tokens
+                 
+
+    def tokenize_number(self):
+        number_string = ""
+        while self.current_char.isdigit():
+            number_string += self.current_char
+            self.next_char()
+
+        self.add_token(TokenType["NUMBER"], int(number_string))
+
+    def tokenize_operator(self):
+        self.add_token(TokenType[self.current_char], self.current_char)
+        self.next_char()
+
+    def next_char(self):
         self.position += 1
-        return self.peek(0)
 
     def peek(self, relative_position):
         position = self.position + relative_position
-        if position >= self.text_lenght:
+        if position >= len(self.text):
             return '\0'
         else:
             return self.text[position]
@@ -37,3 +71,7 @@ class Lexer:
         self.tokens.append(Token(token_type, text))
 
 
+if __name__ == "__main__":
+    input_text = '2+2'
+    lexer = Lexer(input_text)
+    print(lexer.tokenize())
