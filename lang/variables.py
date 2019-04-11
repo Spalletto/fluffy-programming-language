38a547c882982a61d9e16ec87515be7d8752a015
Alex Solonenko <alex.solonencko@gmail.com>
2019-04-12 00:21:09 +0300

    Final version

--- lang/variables.py
+++ lang/variables.py
@@ -1,3 +1,8 @@
+from signals import output
+from shapely.geometry.multipolygon import MultiPolygon
+from shapely.geometry.polygon import Polygon
+
+
 class Variables:
     VARIABLES = {}
 
@@ -11,11 +16,19 @@ class Variables:
         if key in self.VARIABLES:
             self.VARIABLES[key]['value'] = value
         else:
-            type, name = key.split(' ')
-            self.VARIABLES[name] = {
-                'type': type,
-                'value': value
-            }
+            _type, name = key.split(' ')
+            if _type == "figure" and not isinstance(value, (MultiPolygon, Polygon)) or \
+                 _type in ('str', 'int') and _type != value.__class__.__name__:
+
+                output.send("Can't assign '{}' to variable of type '{}'. Inappropriate value for this type.".format(
+                    value, _type
+                ))
+                return
+            else:
+                self.VARIABLES[name] = {
+                    'type': _type,
+                    'value': value
+                }
 
 
 variables = Variables()
